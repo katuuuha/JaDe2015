3c4996871997c0225c709519e6dd28f9053dd977
maadz1 <madara.sneidere@gmail.com>
2015-10-31 17:05:43 +0200

    Customer properties

--- src/vea/itm/jade2015/m01/Customer.java
+++ src/vea/itm/jade2015/m01/Customer.java
@@ -1,8 +1,40 @@
 package vea.itm.jade2015.m01;
 
 public class Customer {
-	public String name;
-    public String stateCode;
-    public String region;
-    public String county;
+	private String name;
+    private String stateCode;
+    private String region;
+    private String county;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStateCode() {
+        return stateCode;
+    }
+
+    public void setStateCode(String stateCode) {
+        this.stateCode = stateCode;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCounty() {
+        return county;
+    }
+
+    public void setCounty(String county) {
+        this.county = county;
+    }
 }
